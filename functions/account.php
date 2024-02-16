2d544ea1fbb2ab6bc36c41c1e383a0c859d406f4
DedeSnrwn13 <sunarwandede13@gmail.com>
2024-02-16 23:18:57 +0700

    fix: select all data candidate

--- functions/account.php
+++ functions/account.php
@@ -15,7 +15,9 @@ function listPreviousVote() {
 
     $loggedInUserId = $userData['id'];
 
-    $stmt = $conn->prepare("SELECT v.id, v.year, c.name AS candidate_name, v.user_id 
+    $stmt = $conn->prepare("SELECT v.id, v.year, c.name AS candidate_name, v.user_id,
+                            c.age, c.program_study, c.short_description, c.vision, c.mission,
+                            c.photo, c.reason_for_choice, c.created_at, c.updated_at 
                             FROM votes v 
                             INNER JOIN candidates c ON v.candidate_id = c.id 
                             WHERE v.user_id = ?");
