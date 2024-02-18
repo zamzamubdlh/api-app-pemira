001e98e610bff3e51329cefd2c8759a95c555c06
DedeSnrwn13 <sunarwandede13@gmail.com>
2024-02-18 07:51:51 +0700

    feat: update account

--- functions/account.php
+++ functions/account.php
@@ -2,7 +2,80 @@
 
 function updateAccount() {
     $conn = connectDB();
-    
+
+    session_start();
+
+    $userData  = checkAuthorization($conn);
+
+    $requestData = json_decode(file_get_contents('php://input'), true);
+
+    if (empty($requestData)) {
+        http_response_code(400);
+        echo json_encode(array("message" => "Tidak ada data yang diterima."));
+        return;
+    }
+
+    $loggedInUserId = $userData['id'];
+
+    $query = "UPDATE users SET";
+    $queryParams = array();
+
+    $newName = $requestData['name'] ?? null;
+    $newEmail = $requestData['email'] ?? null;
+    $newPhone = $requestData['phone'] ?? null;
+    $newAge = $requestData['age'] ?? null;
+
+    if (empty($newName) && empty($newEmail) && empty($newPhone) && empty($newAge)) {
+        http_response_code(400);
+        echo json_encode(array("message" => "Setidaknya satu data yang diperlukan untuk diperbarui."));
+        return;
+    }
+
+    $bindParamTypes = '';
+
+    if (!empty($newName)) {
+        $query .= " name = ?,";
+        $bindParamTypes .= 's';
+        $queryParams[] = $newName;
+    }
+
+    if (!empty($newEmail)) {
+        $query .= " email = ?,";
+        $bindParamTypes .= 's';
+        $queryParams[] = $newEmail;
+    }
+
+    if (!empty($newPhone)) {
+        $query .= " phone = ?,";
+        $bindParamTypes .= 's';
+        $queryParams[] = $newPhone;
+    }
+
+    if (!empty($newAge)) {
+        $query .= " age = ?,";
+        $bindParamTypes .= 'i';
+        $queryParams[] = $newAge;
+    }
+
+    $query = rtrim($query, ",");
+
+    $query .= " WHERE id = ?";
+    $bindParamTypes .= 'i';
+    $queryParams[] = $loggedInUserId;
+
+    $stmt = $conn->prepare($query);
+
+    $stmt->bind_param($bindParamTypes, ...$queryParams);
+
+    if ($stmt->execute()) {
+        http_response_code(200);
+        echo json_encode(array("message" => "Akun berhasil diperbarui."));
+    } else {
+        http_response_code(500);
+        echo json_encode(array("message" => "Gagal memperbarui akun. Silakan coba lagi."));
+    }
+
+    $stmt->close();
     $conn->close();
 }
 
